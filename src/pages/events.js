32dc8a4dc742543d85a6001569efb83d0c3b57f1
JakeL1001 <50351260+JakeL1001@users.noreply.github.com>
2021-11-14 19:23:53 +0800

    Modified for github pages hosting

---
+++ src/pages/events.js
@@ -0,0 +1,12 @@
+
+import React from 'react';
+  
+const Events = () => {
+  return (
+    <div className="page">
+      <h1 className="title">QFin past and future Events</h1>
+    </div>
+  );
+};
+  
+export default Events;
---
+++ src/pages/signup.js
@@ -0,0 +1,12 @@
+
+import React from 'react';
+  
+const SignUp = () => {
+  return (
+    <div className="page">
+      <h1 className="title">QFin Sign Up</h1>
+    </div>
+  );
+};
+  
+export default SignUp;
