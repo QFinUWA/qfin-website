226c38212a0f8dbdc10c5881ef09a833c725d358
JakeL1001 <50351260+JakeL1001@users.noreply.github.com>
2021-11-15 23:02:44 +0800

    Renamed Navbar js file for easier navigation

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import Navbar from './components/Navbar';
+import Navbar from './components/Navbar/Navbar';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/home';
 import About from './pages/about';
