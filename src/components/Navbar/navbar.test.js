1601a905c08d608dafac0dd9ccd463565e2063fd
Arya Gerami Zadegan <aryageza@gmail.com>
2021-11-23 22:59:10 +0800

    Added hamburger testing -- just need to test links work now

--- src/components/Navbar/navbar.test.js
+++ src/components/Navbar/navbar.test.js
@@ -1,16 +1,43 @@
 import React from 'react';
-import { render } from '@testing-library/react';
-import {MemoryRouter} from 'react-router-dom'
+import { render, waitFor, act } from '@testing-library/react';
+// import { act } from 'react-dom/test-utils';
+import { MemoryRouter } from 'react-router-dom'
 import userEvent from '@testing-library/user-event';
 
 import '@testing-library/jest-dom';
 
 import Navbar from './index';
 
+const setWindowWidth = (width) => {
+    global.innerWidth = width;
+    global.dispatchEvent(new Event('resize'));
+}
+
+const setMobile = () => {
+    setWindowWidth(700);
+}
+
+const setDesktop = () => {
+    setWindowWidth(1200);
+}
+
 describe('<Navbar/>', () => {
-    it('matches the snapshot', () => {
+    it('matches the desktop snapshot', () => {
         const { container } = render(<Navbar />, { wrapper: MemoryRouter });
         expect(container).toMatchSnapshot();
+    });
+
+    it('matches the mobile snapshot', () => {
+        const { container } = render(<Navbar />, { wrapper: MemoryRouter });
+        act(() => {
+            setMobile();
+        });
+
+        expect(container).toMatchSnapshot();
+
+        act(() => {
+            setDesktop();
+        });
     });
 
     it('renders with all menu items on desktop', () => {
@@ -19,4 +46,40 @@ describe('<Navbar/>', () => {
         expect(navbar.childElementCount).toBe(2);
         expect(navbar.children[1].childElementCount).not.toBe(1);
     });
+
+    it('renders with hamburger menu on mobile', () => {
+        const { getByTestId } = render(<Navbar />, { wrapper: MemoryRouter });
+        act(() => {
+            setMobile();
+        });
+        
+        const navbar = getByTestId('nav');
+        expect(navbar.childElementCount).toBe(2);
+        expect(navbar.children[1].childElementCount).toBe(1);
+
+        act(() => {
+            setDesktop();
+        });
+    })
+
+    it('hamburger menu opens and collapses menu', async () => {
+        const { getByTestId } = render(<Navbar />, { wrapper: MemoryRouter });
+        act(() => {
+            setMobile();
+        });
+        
+        expect(getByTestId('nav').childElementCount).toBe(2);
+        expect(getByTestId('nav').children[1].childElementCount).toBe(1);
+
+        const hamburgerButton = getByTestId('nav').children[1];
+        userEvent.click(hamburgerButton);
+        await waitFor(() => expect(getByTestId('nav').childElementCount).toBe(3));
+
+        userEvent.click(hamburgerButton);
+        await waitFor(() => expect(getByTestId('nav').childElementCount).toBe(2));
+
+        act(() => {
+            setDesktop();
+        });
+    })
 })
