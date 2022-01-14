d731d077acdea392d996c66db71461c243b35f77
JakeL1001 <50351260+JakeL1001@users.noreply.github.com>
2022-01-15 00:47:38 +0800

    Edited Projects Page

--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -7,22 +7,25 @@ const Projects = () => {
   return (
     <div className="page">
       <h1 className="title">QFin Projects</h1>
-      <p className="content">QFin's trading team is tasked with completing projects devised by the Director of Trading each semester. These projects are based around the core concepts of quantitative analysis and mathematical theory.</p>
+      <p className="content">QFin's trading team is tasked with completing projects devised by the Director of Trading each semester. 
+                            These projects are based around the core concepts of quantitative analysis and mathematical theory.
+                            </p>
       <div className="ProjectContainer">
         <div className="ProjectYear">
           <h2 className="ProjectYearTitle">2022</h2>
-          Semester 2 {/* maybe exclude */}
+          {/* Semester 2 maybe exclude
           <Project title="TBD"
             desc="TBD"
             tools="TBD"
             results="'TBD'"
-            repo="TBD" />
+            repo="TBD" /> */}
           Semester 1
-          <Project title="TBD"
-            desc="TBD"
-            tools="TBD"
-            results="'TBD'"
-            repo="TBD" />        </div>
+          <Project title="Mean Reversion"
+            desc="This Project will be focused on the theory of mean reversion. Teams will be given a set of historical data and will be tasked with finding the optimal parameters for a mean reversion model."
+            tools="Python, Pandas, Vectorbt"
+            results="TBD"
+            repo="TBD" />        
+          </div>
         <div className="ProjectYear">
           <h2 className="ProjectYearTitle">2021</h2>
           Semester 2 {/* maybe exclude */}
@@ -34,7 +37,7 @@ const Projects = () => {
           Semester 1
           <Project title="Pairs Trading"
             desc="This project was based on the concept of pairs trading"
-            tools="Quant Connect"
+            tools="Python, Quant Connect"
             results="'Project report link'"
             repo="https://github.com/QFinUWA/Project-Results/tree/main/2021/Semester1-Pairs-Trading" />
 
