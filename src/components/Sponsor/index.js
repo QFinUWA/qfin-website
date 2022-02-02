2b79e2bbbb808405681eb85119f86e4342870b6c
JakeL1001 <me@jakelyell.dev>
2022-02-02 12:39:05 +0800

    update to sponsors

--- src/components/Sponsor/index.js
+++ src/components/Sponsor/index.js
@@ -4,10 +4,12 @@ const Sponsor = (children) => {
     return (
         <>
             <div className="Sponsor">
+                <div className="Sponsor-logo-container">
+                    {children.logo.length > 0 &&
+                    <img className="Sponsor-logo" src={children.logo} alt={children.name} />
+                    }
+                </div>
                 <h2 className="Sponsor-name">{children.name}</h2>
-                {children.logo.length > 0 &&
-                <img className="Sponsor-logo" src={children.logo} alt={children.name} />
-                }
                 <p className="Sponsor-desc"> <b>Description</b>: {children.desc}</p>
                 {children.website.length > 5 &&
                 <p className="Sponsor-website"><b>Sponsor Site</b>: <a href={children.website} target="_blank" rel="noopener noreferrer"> {children.name} </a></p>
--- src/pages/sponsors/index.js
+++ src/pages/sponsors/index.js
@@ -1,19 +1,42 @@
 import React from 'react';
 import Sponsor from '../../components/Sponsor';
+import IMCLogo from '../../components/assets/sponsor-images/IMCLogo.png';
+import JaneStreetLogo from '../../components/assets/sponsor-images/JaneStreetLogo.png';
+import JaneStreetLogo2 from '../../components/assets/sponsor-images/JaneStreetLogo2.png';
+import SIGLogo from '../../components/assets/sponsor-images/SIGLogo.png';
   
 const Sponsors = () => {
   return (
     <div className="page">
       <h1 className="title">QFin's 2022 Sponsors</h1>
       <div className="SponsorsContainer">
+      <hr />
+      <h2 className="Sponsor-type" style={{fontSize:"3rem"}}>Platinum Sponsor</h2>
         <div className="PlatinumSponsor">
-          <h2 className="Sponsor-type">Platinum Sponsor</h2>
           <Sponsor 
-          name="IMG"
-          logo=""
+          name="IMC Trading"
+          logo={IMCLogo}
           desc="test"
-          website="siteee"/>
+          website="https://www.imc.com/ap/"/>
         </div>
+        <hr />
+        <h2 className="Sponsor-type">Gold Sponsors</h2>
+        <div className="GoldSponsors">
+          <Sponsor
+          name="Jane Street Capital"
+          logo={JaneStreetLogo2}
+          desc="test"
+          website="https://www.janestreet.com/"/>
+
+          <Sponsor
+          name="Susquehanna International Group"
+          logo={SIGLogo}
+          desc="test"
+          website="https://sig.com/"/>
+        </div>
+        {/* <hr />
+        <h2 className="Sponsor-type">Silver Sponsors</h2>
+        <div className="SilverSponsors"></div> */}
       </div>
     </div>
   );
