90a96293e7e9caf3a8084c31e7e3476032ad221b
JakeL1001 <me@jakelyell.dev>
2022-03-22 23:01:45 +0800

    Sponsor logo invert

--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -4,6 +4,7 @@ import React from 'react';
 
 import MOMENTUM from '../../components/assets/project-images/Momentum.png';
 import PAIRS from '../../components/assets/project-images/Pairs.png';
+import REVERSION from '../../components/assets/project-images/Reversion.png';
 
 import { Fade } from "react-awesome-reveal";
 
@@ -13,11 +14,11 @@ const Projects = () => {
     <div className="page">
       <h1 className="title">QFin Trading Team Projects</h1>
       <p className="content">Each Semester, the Executive Director of Trading devises a project for the QFin Trading Teams to complete throughout
-                            the semester. The project is based around the core concepts of quantitative analysis and mathematical theory.
-                            So far, the projects have primarily consisted of backtesting trading strategies against historical stock and crypto data,
-                            with the goal of determining the best trading strategy across a range of stocks and crypto assets and optimizing that strategy.
-      
-      {/* <div className="card" style={{width:"70%", margin:"20px auto", borderRadius:"5px", backgroundColor:"rgb(63,63,63)", padding:"5px"}}>
+        the semester. The project is based around the core concepts of quantitative analysis and mathematical theory.
+        So far, the projects have primarily consisted of backtesting trading strategies against historical stock and crypto data,
+        with the goal of determining the best trading strategy across a range of stocks and crypto assets and optimizing that strategy.
+
+        {/* <div className="card" style={{width:"70%", margin:"20px auto", borderRadius:"5px", backgroundColor:"rgb(63,63,63)", padding:"5px"}}>
         <p className="content1" style={{textAlign:"center",fontWeight:"bold"}}>Do any of these Projects interest you? Applications are now open for 2022's Trading Team!</p>
         <p className="content1" style={{textAlign:"center"}}>Applications close 11:59PM Sunday, March 6th</p>
         <p className="content1" style={{textAlign:"center"}}>Trading Team Description: <a href='https://bit.ly/36H8o0b' target="_blank" rel="noreferrer">Trading Team Member</a></p>
@@ -27,7 +28,7 @@ const Projects = () => {
       </p>
       <div className="ProjectContainer">
         <div className="ProjectYear">
-        {/* <hr className='Main-separator' /> */}
+          {/* <hr className='Main-separator' /> */}
           <h2 className="ProjectYearTitle">2022</h2>
           {/* Semester 2 maybe exclude */}
           {/* <Project title="TBD"
@@ -36,14 +37,14 @@ const Projects = () => {
             tools="TBD"
             repolink="TBD" /> */}
           <Fade duration={2000} triggerOnce="true">
-          <Project title="Mean Reversion - Semester 1"
-            image="" // Add image of workshop?
-            DOT="Jake Lyell"
-            DOTlink="https://www.linkedin.com/in/jake-lyell/"
-            desc="This Project will be focused on the theory of mean reversion. Teams will be given a set of historical data and will be tasked with finding the optimal parameters for a mean reversion model."
-            tools="Python, Pandas, Custom Backtesting Module"
-            repolink="TBD" />     
-          </Fade>   
+            <Project title="Mean Reversion - Semester 1"
+              image={REVERSION}
+              DOT="Jake Lyell"
+              DOTlink="https://www.linkedin.com/in/jake-lyell/"
+              desc="This Project will be focused on the theory of mean reversion. Teams will be given a set of historical data and will be tasked with finding the optimal parameters for a mean reversion model."
+              tools="Python, Pandas, Custom Backtesting Module"
+              repolink="TBD" />
+          </Fade>
         </div>
         {/* <hr className='Section-separator' /> */}
         {/* <hr></hr> */}
@@ -52,26 +53,26 @@ const Projects = () => {
           {/* Semester 2 */}
           {/* <hr className='Sub-separator' /> */}
           <Fade duration={2000} triggerOnce="true">
-          <Project title="Momentum Trading - Semester 2"
-            image={MOMENTUM}
-            DOT="Kane Alexander"
-            DOTlink="https://www.linkedin.com/in/kane-alexander-08131b216/"
-            desc="This project was based on the Quantitative concept of Momentum Trading. 
+            <Project title="Momentum Trading - Semester 2"
+              image={MOMENTUM}
+              DOT="Kane Alexander"
+              DOTlink="https://www.linkedin.com/in/kane-alexander-08131b216/"
+              desc="This project was based on the Quantitative concept of Momentum Trading. 
                   The goal of this project was to determine the best trading strategy based on stock price momentum for a set of historical data of popular cryptocurrencies.
                   The project was completed by 4 teams of 3 people."
-            tools="Python, Pandas, Gemini Modules"
-            repolink="https://github.com/QFinUWA/Project-Results/tree/main/2021/Semester2-Momentum-Trading" />
+              tools="Python, Pandas, Gemini Modules"
+              repolink="https://github.com/QFinUWA/Project-Results/tree/main/2021/Semester2-Momentum-Trading" />
           </Fade>
           {/* Semester 1 */}
           {/* <hr className='Sub-separator' /> */}
           <Fade duration={2000} triggerOnce="true">
-          <Project title="Pairs Trading - Semester 1"
-            image={PAIRS}
-            DOT="Kane Alexander"
-            DOTlink="https://www.linkedin.com/in/kane-alexander-08131b216/"
-            desc="This project was based on the concept of Pairs Trading. The goal of this project was to research potential stock pairs and then design, implement, and backtest an algorithm to trade the pairs. The project was completed by 3 teams of 4 people."
-            tools="Python, Quant Connect"
-            repolink="https://github.com/QFinUWA/Project-Results/tree/main/2021/Semester1-Pairs-Trading" />
+            <Project title="Pairs Trading - Semester 1"
+              image={PAIRS}
+              DOT="Kane Alexander"
+              DOTlink="https://www.linkedin.com/in/kane-alexander-08131b216/"
+              desc="This project was based on the concept of Pairs Trading. The goal of this project was to research potential stock pairs and then design, implement, and backtest an algorithm to trade the pairs. The project was completed by 3 teams of 4 people."
+              tools="Python, Quant Connect"
+              repolink="https://github.com/QFinUWA/Project-Results/tree/main/2021/Semester1-Pairs-Trading" />
           </Fade>
           {/* <hr className='Sub-separator' /> */}
           {/* <hr className='Main-separator' /> */}
--- src/pages/sponsors/index.js
+++ src/pages/sponsors/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 import Sponsor from '../../components/Sponsor';
 import IMCLogo from '../../components/assets/sponsor-images/IMCLogo.png';
 // import JaneStreetLogo from '../../components/assets/sponsor-images/JaneStreetLogo.png';
-import JaneStreetLogo2 from '../../components/assets/sponsor-images/JaneStreetLogo2.png';
-import SIGLogo from '../../components/assets/sponsor-images/SIGLogo.png';
-import OptiverLogo from '../../components/assets/sponsor-images/Optiver.png';
+import JaneStreetLogo2Inverted from '../../components/assets/sponsor-images/JaneStreetLogo2Inverted.png';
+import SIGLogoInverted from '../../components/assets/sponsor-images/SIGLogoInverted.png';
+import OptiverLogoInverted from '../../components/assets/sponsor-images/OptiverInverted.png';
 import SponsorTiers from "../../components/assets/Documents/QFin Sponsorship Tiers.pdf";
 
 import { Fade } from "react-awesome-reveal";
@@ -13,42 +13,42 @@ const Sponsors = () => {
   return (
     <div className="page">
       <h1 className="title">QFin's 2022 Sponsors</h1>
-      <p style={{textAlign:"center"}} className="content">QFin's Sponsors for 2022, providing us with financial support to run our events and projects throughout the year.</p>
-      <p className="content" style={{textAlign:"center", fontWeight:"bold", margin:"10px"}}><a href={SponsorTiers} download="QFin Sponsorship Tiers">Our Sponsorship Tiers</a></p>
+      <p style={{ textAlign: "center" }} className="content">QFin's Sponsors for 2022, providing us with financial support to run our events and projects throughout the year.</p>
+      <p className="content" style={{ textAlign: "center", fontWeight: "bold", margin: "10px" }}><a href={SponsorTiers} download="QFin Sponsorship Tiers">Our Sponsorship Tiers</a></p>
       <div className="SponsorsContainer">
-      <hr />
-      <h2 className="Sponsor-type" style={{fontSize:"3rem"}}>Platinum Sponsor</h2>
-      <Fade duration={2000} triggerOnce="true">
-        <div className="PlatinumSponsor">
-          <Sponsor 
-          name="IMC Trading"
-          logo={IMCLogo}
-          desc="Our Platinum tier sponsor is IMC Trading, a leading proprietary trading firm globally and a key market maker in various products listed on exchanges throughout the world. As a pathfinder in advanced technology, IMC values expertise and creativity in its workplace, and takes pride in creating an open culture where great ideas thrive. "
-          website="https://www.imc.com/ap/"/>
-        </div>
-      </Fade>
+        <hr />
+        <h2 className="Sponsor-type" style={{ fontSize: "3rem" }}>Platinum Sponsor</h2>
+        <Fade duration={2000} triggerOnce="true">
+          <div className="PlatinumSponsor">
+            <Sponsor
+              name="IMC Trading"
+              logo={IMCLogo}
+              desc="Our Platinum tier sponsor is IMC Trading, a leading proprietary trading firm globally and a key market maker in various products listed on exchanges throughout the world. As a pathfinder in advanced technology, IMC values expertise and creativity in its workplace, and takes pride in creating an open culture where great ideas thrive. "
+              website="https://www.imc.com/ap/" />
+          </div>
+        </Fade>
         <hr />
         <h2 className="Sponsor-type">Gold Sponsors</h2>
         <Fade duration={2000} triggerOnce="true">
-        <div className="GoldSponsors">
-          <Sponsor
-          name="Jane Street Capital"
-          logo={JaneStreetLogo2}
-          desc="Jane Street is one of the largest ETF liquidity providers in the world and excels at executing large trades with minimal market impact."
-          website="https://www.janestreet.com/"/>
+          <div className="GoldSponsors">
+            <Sponsor
+              name="Jane Street Capital"
+              logo={JaneStreetLogo2Inverted}
+              desc="Jane Street is one of the largest ETF liquidity providers in the world and excels at executing large trades with minimal market impact."
+              website="https://www.janestreet.com/" />
 
-          <Sponsor
-          name="Susquehanna International Group"
-          logo={SIGLogo}
-          desc="SIG is a global quantitative trading firm with a broad presence in the market, specialising in trading with quantitative research and innovative technology. "
-          website="https://sig.com/"/>
+            <Sponsor
+              name="Susquehanna International Group"
+              logo={SIGLogoInverted}
+              desc="SIG is a global quantitative trading firm with a broad presence in the market, specialising in trading with quantitative research and innovative technology. "
+              website="https://sig.com/" />
 
-          <Sponsor
-          name="Optiver"
-          logo={OptiverLogo}
-          desc="Optiver is one of the oldest market making institutions dedicated to making markets more liquid and efficient by providing competitive bid-ask prices across a range of financial products."
-          website="https://www.optiver.com/"/>
-        </div>
+            <Sponsor
+              name="Optiver"
+              logo={OptiverLogoInverted}
+              desc="Optiver is one of the oldest market making institutions dedicated to making markets more liquid and efficient by providing competitive bid-ask prices across a range of financial products."
+              website="https://www.optiver.com/" />
+          </div>
         </Fade>
         {/* <hr />
         <h2 className="Sponsor-type">Silver Sponsors</h2>
@@ -57,5 +57,5 @@ const Sponsors = () => {
     </div>
   );
 };
-  
+
 export default Sponsors;
