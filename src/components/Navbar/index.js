55370a518b4917da656c28d0bc2d745e9b2d14af
cookies-xor-cream <aryageza@gmail.com>
2022-07-12 19:19:37 +0800

    Nav responsive without JS: added animation

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -11,13 +11,6 @@ import {
 const Navbar = () => {
 
   const [burgerOpen, setBurgerOpen] = React.useState(false);
-  const [isMobile, setIsMobile] = React.useState(false);
-  const onWindowResize = () => setIsMobile(window.innerWidth < 768);
-
-  useEffect(() => {
-    onWindowResize();
-    window.onresize = onWindowResize;
-  }, []);
 
   const toggleBurger = () => {
     setBurgerOpen(!burgerOpen);
@@ -33,50 +26,44 @@ const Navbar = () => {
         <NavQFin className="QFin" to="/home" onClick={closeBurger} activestyle="true">
           QFin UWA
         </NavQFin>
-        {isMobile &&
-          <Bars className="burger" onClick={toggleBurger}/>
-        }
-        {(!isMobile) &&
-          <NavMenu>
-            {/* <NavLink to='/about' activestyle="true">
-              About
-            </NavLink> */}
-            <NavLink to='/events' activestyle="true">
-              Events
-            </NavLink>
-            <NavLink to='/projects' activestyle="true">
-              Projects
-            </NavLink>
-            <NavLink to='/team' activestyle="true">
-              Team
-            </NavLink>
-            <NavLink to='/sponsors' activestyle="true">
-              Sponsors
-            </NavLink>
-            <a href='https://forms.gle/LjNMSxCUY9ytyvJ97' target="blank_" style={{textDecoration: "none"}}><div className='fakeNavLink'>Sign Up</div> </a>
-          </NavMenu>
-        }
+        <Bars className="burger" onClick={toggleBurger}/>
+        <NavMenu>
+          <NavLink to='/about' activestyle="true">
+            About
+          </NavLink>
+          <NavLink to='/events' activestyle="true">
+            Events
+          </NavLink>
+          <NavLink to='/projects' activestyle="true">
+            Projects
+          </NavLink>
+          <NavLink to='/team' activestyle="true">
+            Team
+          </NavLink>
+          <NavLink to='/sponsors' activestyle="true">
+            Sponsors
+          </NavLink>
+          <a href='https://forms.gle/LjNMSxCUY9ytyvJ97' target="blank_" style={{textDecoration: "none"}}><div className='fakeNavLink'>Sign Up</div> </a>
+        </NavMenu>
       </Nav>
-      {(burgerOpen && isMobile) &&
-          <BurgerNavMenu>
-            <NavLink to='/about'  onClick={toggleBurger} activestyle="true">
-              About
-            </NavLink>
-            <NavLink to='/projects' onClick={toggleBurger} activestyle="true">
-              Projects
-            </NavLink>
-            <NavLink to='/events' onClick={toggleBurger} activestyle="true">
-              Events
-            </NavLink>
-            <NavLink to='/team' onClick={toggleBurger} activestyle="true">
-              Team
-            </NavLink>
-            <NavLink to='/sponsors' onClick={toggleBurger} activestyle="true">
-              Sponsors
-            </NavLink>
-            <a href='https://forms.gle/LjNMSxCUY9ytyvJ97' target="blank_" style={{textDecoration: "none"}}><div className='fakeNavLink'>Sign Up</div> </a>
-          </BurgerNavMenu>
-        }
+      <BurgerNavMenu burgerOpen={burgerOpen}>
+        <NavLink to='/about'  onClick={toggleBurger} activestyle="true">
+          About
+        </NavLink>
+        <NavLink to='/projects' onClick={toggleBurger} activestyle="true">
+          Projects
+        </NavLink>
+        <NavLink to='/events' onClick={toggleBurger} activestyle="true">
+          Events
+        </NavLink>
+        <NavLink to='/team' onClick={toggleBurger} activestyle="true">
+          Team
+        </NavLink>
+        <NavLink to='/sponsors' onClick={toggleBurger} activestyle="true">
+          Sponsors
+        </NavLink>
+        <a href='https://forms.gle/LjNMSxCUY9ytyvJ97' target="blank_" style={{textDecoration: "none"}}><div className='fakeNavLink'>Sign Up</div> </a>
+      </BurgerNavMenu>
     </>
   );
 };
